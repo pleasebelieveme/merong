6337569654846083ed23c199663ec331ca032ab3
pleasebelieveme <cain1227@naver.com>
2025-05-16 14:24:22 +0900

    init: 초기 설정 수정

---
+++ src/test/java/org/example/merong/MerongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.merong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
