7ada5f027be45aa4428fb52349bcc89b658f235b
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-19 15:04:34 +0900

    feat: CommentRepository 기능 추가
    commentId 와 userId에 일치하는 comment 가져오는 코드 추가

--- src/main/java/org/example/merong/domain/comments/repository/CommentRepository.java
+++ src/main/java/org/example/merong/domain/comments/repository/CommentRepository.java
@@ -1,8 +1,17 @@
 package org.example.merong.domain.comments.repository;
 
+import java.util.Optional;
 import org.example.merong.domain.comments.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    Optional<Comment> findByIdAndUserId(Long commentId,Long userId);
+
+    default Comment findByIdAndUserIdOrElseThrow(Long commentId,Long userId){
+        return findByIdAndUserId(commentId, userId)
+                .orElseThrow(()->
+                        new IllegalArgumentException("존재하지 않는 댓글입니다.")
+                );
+    }
 }
