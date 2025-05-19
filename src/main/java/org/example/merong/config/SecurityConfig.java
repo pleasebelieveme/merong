970a4a3494f3770a86b55a8a3e8bf2a98bb18bae
syji <jrl10346@gmail.com>
2025-05-19 17:10:34 +0900

    refactor: CORS 설정 PATCH 추가

--- src/main/java/org/example/merong/config/SecurityConfig.java
+++ src/main/java/org/example/merong/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
