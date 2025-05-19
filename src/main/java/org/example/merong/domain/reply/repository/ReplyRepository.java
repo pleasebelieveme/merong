406394027742b059317174f3cdde06c18a597422
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-19 19:10:17 +0900

    feat: replyrepository 생성

---
+++ src/main/java/org/example/merong/domain/reply/repository/ReplyRepository.java
@@ -0,0 +1,8 @@
+package org.example.merong.domain.reply.repository;
+
+import org.example.merong.domain.reply.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+}
