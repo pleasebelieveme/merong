43e8b44bd844c0a5bd62f19099dbffe7dbadf30e
pleasebelieveme <cain1227@naver.com>
2025-05-19 20:56:05 +0900

    refactor: auth 적용

--- src/main/java/org/example/merong/domain/likes/controller/LikeController.java
+++ src/main/java/org/example/merong/domain/likes/controller/LikeController.java
@@ -1,14 +1,17 @@
 package org.example.merong.domain.likes.controller;
 
+import org.example.merong.domain.auth.dto.UserAuth;
 import org.example.merong.domain.likes.dto.request.LikeRequestDto;
 import org.example.merong.domain.likes.dto.response.LikeResponseDto;
 import org.example.merong.domain.likes.service.LikeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,18 +25,22 @@ public class LikeController {
 	private final LikeService likeService;
 
 	// 좋아요 추가
-	@PostMapping
+	@PostMapping("/{songId}")
 	public ResponseEntity<LikeResponseDto.Add> doLike(
-		@RequestBody LikeRequestDto.Add dto
+		@RequestParam Long songId,
+		@AuthenticationPrincipal UserAuth userAuth
 	){
-		LikeResponseDto.Add responseDto = likeService.doLike(dto);
+		LikeResponseDto.Add responseDto = likeService.doLike(songId, userAuth.getId());
 		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 
 	// 2. 좋아요 취소
-	@DeleteMapping
-	public ResponseEntity<Void> unLike(@RequestBody LikeRequestDto.Add dto) {
-		likeService.unLike(dto);
+	@DeleteMapping("/{songId}")
+	public ResponseEntity<Void> unLike(
+		@RequestParam Long songId,
+		@AuthenticationPrincipal UserAuth userAuth
+	) {
+		likeService.unLike(songId, userAuth.getId());
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
