8919b2c2d8ccaf8f97c107a90276ec78b55570d8
pleasebelieveme <cain1227@naver.com>
2025-05-20 23:56:44 +0900

    wip: 복구된 작업 백업

--- src/main/java/org/example/merong/domain/likes/controller/LikeController.java
+++ src/main/java/org/example/merong/domain/likes/controller/LikeController.java
@@ -1,13 +1,14 @@
 package org.example.merong.domain.likes.controller;
 
-import org.example.merong.domain.likes.dto.request.LikeRequestDto;
+import org.example.merong.domain.auth.dto.UserAuth;
 import org.example.merong.domain.likes.dto.response.LikeResponseDto;
 import org.example.merong.domain.likes.service.LikeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/likes")
+@RequestMapping("/api/songs/{songId}/likes")
 public class LikeController {
 
 	private final LikeService likeService;
@@ -24,16 +25,21 @@ public class LikeController {
 	// 좋아요 추가
 	@PostMapping
 	public ResponseEntity<LikeResponseDto.Add> doLike(
-		@RequestBody LikeRequestDto.Add dto
+		@PathVariable Long songId,
+		@AuthenticationPrincipal UserAuth userAuth
 	){
-		LikeResponseDto.Add responseDto = likeService.doLike(dto);
+		LikeResponseDto.Add responseDto = likeService.doLike(songId, userAuth.getId());
 		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 
 	// 2. 좋아요 취소
-	@DeleteMapping
-	public ResponseEntity<Void> unLike(@RequestBody LikeRequestDto.Add dto) {
-		likeService.unLike(dto);
+	@DeleteMapping("/{likeId}")
+	public ResponseEntity<Void> unLike(
+		@PathVariable Long songId,
+		@PathVariable Long likeId,
+		@AuthenticationPrincipal UserAuth userAuth
+	) {
+		likeService.unLike(songId, likeId, userAuth.getId());
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
--- src/main/java/org/example/merong/domain/likes/service/LikeService.java
+++ src/main/java/org/example/merong/domain/likes/service/LikeService.java
@@ -3,6 +3,8 @@ package org.example.merong.domain.likes.service;
 import org.example.merong.domain.likes.dto.request.LikeRequestDto;
 import org.example.merong.domain.likes.dto.response.LikeResponseDto;
 import org.example.merong.domain.likes.entity.Like;
+import org.example.merong.domain.likes.exception.LikeException;
+import org.example.merong.domain.likes.exception.LikeExceptionCode;
 import org.example.merong.domain.likes.repository.LikeRepository;
 import org.example.merong.domain.songs.SongRepository;
 import org.example.merong.domain.songs.entity.Song;
@@ -21,21 +23,20 @@ public class LikeService {
 	private final UserRepository userRepository;
 	private final SongRepository songRepository;
 
-	public LikeResponseDto.Add doLike(LikeRequestDto.Add dto) {
+	public LikeResponseDto.Add doLike(Long songId, Long userId) {
 
 		// 중복 좋아요 체크
-		boolean exists = likeRepository.existsByUserIdAndSongId(dto.getUserId(), dto.getSongId());
+		boolean exists = likeRepository.existsByUserIdAndSongId(songId, userId);
 		if (exists) {
-			throw new IllegalStateException("이미 좋아요를 눌렀습니다.");
+			throw new LikeException(LikeExceptionCode.LIKE_NOT_FOUND);
 		}
 
 		// 유저 존재유무
-		User findUser = userRepository.findById(dto.getUserId())
+		User findUser = userRepository.findById(userId)
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
 		// 노래 존재유무
-		Song findSong = songRepository.findById(dto.getSongId())
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 노래입니다."));
+		Song findSong = songRepository.findByIdOrElseThrow(songId);
 
 		Like like = Like.builder()
 			.user(findUser)
@@ -48,10 +49,10 @@ public class LikeService {
 	}
 
 	@Transactional
-	public void unLike(LikeRequestDto.Add dto) {
+	public void unLike(Long songId, Long likeId, Long userId) {
 
 		// 좋아요가 존재하는지 확인
-		Like findLike = likeRepository.findByUserIdAndSongIdOrElseThrow(dto.getUserId(), dto.getSongId());
+		Like findLike = likeRepository.findByUserIdAndSongIdOrElseThrow(songId, userId);
 
 		likeRepository.delete(findLike);
 	}
