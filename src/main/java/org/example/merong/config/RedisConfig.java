35e7275f6350d2a513544840450ed3ddb154c362
syji <jrl10346@gmail.com>
2025-05-22 00:52:16 +0900

    feat: In-memory Cache 가 적용된 v2 검색 API 를 Redis 를 이용한 Remote Cache 로 수정

--- src/main/java/org/example/merong/config/RedisConfig.java
+++ src/main/java/org/example/merong/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+// RedisTemplate 을 위한 설정
 @Configuration
 public class RedisConfig {
 
