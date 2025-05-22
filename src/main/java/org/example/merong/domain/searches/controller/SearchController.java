4d76d9e1b039db0c04b85a83e5b900fe5a762e80
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-22 11:11:57 +0900

    feat: 인기 검색어 기능

---
+++ src/main/java/org/example/merong/domain/searches/controller/SearchController.java
@@ -0,0 +1,34 @@
+package org.example.merong.domain.searches.controller;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.merong.domain.searches.dto.SearchResponseDto;
+import org.example.merong.domain.searches.repository.SearchRepository;
+import org.example.merong.domain.searches.service.SearchService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping("/api/songs/search/popular")
+    public ResponseEntity<Page<SearchResponseDto>> findPopular(
+            @PageableDefault(size = 10, sort = "count", direction = Sort.Direction.DESC) Pageable pageable
+    ){
+
+        Page<SearchResponseDto> popular = searchService.findPopular(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(popular);
+    }
+
+}
