60ade82f420a90e6e504092fbe009fe8495ca849
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-22 11:12:23 +0900

    feat: 인기 검색어 기능

---
+++ src/main/java/org/example/merong/domain/searches/dto/SearchResponseDto.java
@@ -0,0 +1,14 @@
+package org.example.merong.domain.searches.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchResponseDto {
+
+    private String keyword;
+
+}
